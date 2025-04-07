2aa910bc85017cafea3b468d438ba38f547620eb
Blinde16 <blakejlinde@gmail.com>
2025-04-07 16:11:07 -0600

    Added the Get requests for the form add, update and delete

--- frontend/src/components/MoviesList.tsx
+++ frontend/src/components/MoviesList.tsx
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Movie } from "../types/Movie";
 
-function MoviesList({
-  selectedTypes,
-}: {
-  selectedTypes: string[];
-}) {
+function MoviesList({ selectedTypes }: { selectedTypes: string[] }) {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [pageSize, setPageSize] = useState<number>(10);
   const [pageNum, setPageNum] = useState<number>(1);
@@ -15,23 +11,24 @@ function MoviesList({
   const navigate = useNavigate();
 
   useEffect(() => {
-      const fetchCompetition = async () => {
-        const containerParams = selectedTypes
-          .map((cont) => `containers=${encodeURIComponent(cont)}`)
-          .join("&");
-        
-        const response = await fetch(
-          `https://localhost:5000/Competition/GetRootbeers?pageSize=${pageSize}&pageNum=${pageNum}${selectedTypes.length ? `&${containerParams}` : ""}`,
-          {
-            credentials: "include",
-          }
-        );
-  
-        const data = await response.json();
-  
-        setMovies(data.brews);
-        setTotalItems(data.totalNumProjects);
-        setTotalPages(Math.ceil(totalItems / pageSize));
-      };
-      fetchCompetition();
-    }, [pageSize, pageNum, totalItems, selectedTypes]);
+    const fetchCompetition = async () => {
+      const containerParams = selectedTypes
+        .map((cont) => `containers=${encodeURIComponent(cont)}`)
+        .join("&");
+
+      const response = await fetch(
+        `https://localhost:5000/Movie/GetMovies?pageSize=${pageSize}&pageNum=${pageNum}${selectedTypes.length ? `&${containerParams}` : ""}`,
+        {
+          credentials: "include",
+        }
+      );
+
+      const data = await response.json();
+
+      setMovies(data.brews);
+      setTotalItems(data.totalNumProjects);
+      setTotalPages(Math.ceil(totalItems / pageSize));
+    };
+    fetchCompetition();
+  }, [pageSize, pageNum, totalItems, selectedTypes]);
+}
