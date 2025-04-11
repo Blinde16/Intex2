55f648654e77b599909be4d81c2d63856e84520d
Blinde16 <blakejlinde@gmail.com>
2025-04-11 08:05:18 -0600

    adding email to movies to ensure authenticated

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,37 +2,37 @@ import { Link } from "react-router-dom";
 import "./css/Header.css";
 import Logout from "./Logout";
 import { useContext } from "react";
-import { UserContext } from "./AuthorizeView"; // make sure to export UserContext from AuthorizeView.tsx
+import { UserContext } from "./AuthorizeView"; // make sure to import UserContext
 
 function Header() {
-  const user = useContext(UserContext);
+  const user = useContext(UserContext); // Get user info from context
 
   const isAdmin = user?.email === "admin@nichemovies.com"; // Replace with your actual admin email
+  const username = user?.email; // Assuming email is being used as username
 
   return (
-    <>
-      <div className="header-bar">
+    <div className="header-bar">
+      <Link to="/movie">
+        <img src="/logo.png" className="header-image" />
+      </Link>
+      <h1>{username ? `Welcome ${username}` : "Welcome"}</h1>{" "}
+      {/* Display username */}
+      <div className="button-container">
         <Link to="/movie">
-          <img src="/logo.png" className="header-image" />
+          <button className="logout-button">Home</button>
         </Link>
-        <h1></h1>
-        <div className="button-container">
-          <Link to="/movie">
-            <button className="logout-button">Home</button>
+        {isAdmin && (
+          <Link to="/AdminPage">
+            <button className="logout-button">Admin</button>
           </Link>
-          {isAdmin && (
-            <Link to="/AdminPage">
-              <button className="logout-button">Admin</button>
-            </Link>
-          )}
-          <Logout>
-            <Link to="/">
-              <button className="logout-button-1"> Logout</button>
-            </Link>
-          </Logout>
-        </div>
+        )}
+        <Logout>
+          <Link to="/">
+            <button className="logout-button-1">Logout</button>
+          </Link>
+        </Logout>
       </div>
-    </>
+    </div>
   );
 }
 
